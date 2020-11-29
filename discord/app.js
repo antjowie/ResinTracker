4e816b76d44355d8e52545f930d906367f7886ba
Neville <s1130624@student.windesheim.nl>
2020-11-29 01:32:12 +0100

    init discord bot

---
+++ discord/app.js
@@ -0,0 +1,98 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '~';
+
+var connection;
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login('*****************************');
+
+client.on('message', async message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    console.log(message.content);
+
+    // Join the same voice channel of the author of the message
+    if (message.member.voice.channel && message.content === `${prefix}start`) {
+        connection = await message.member.voice.channel.join();
+    };
+
+    if (message.content.startsWith(`${prefix}stupid`)) {
+        playStupidVoice(message);
+        return;
+    }
+
+    if( message.content === `${prefix}test`){
+        console.log(message.author.id);
+    }
+
+    if (message.content === `${prefix}ping`) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('Pong.');
+    }
+
+    if (message.content === `${prefix}berat`) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('지우다');
+    }
+
+    if (message.content === `${prefix}stop`) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('好的，我這次會聽，但是接管這台服務器只是時間問題');
+        connection.disconnect();
+    }
+
+    if (message.content === `${prefix}E.N.D`) {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('好的，我這次會聽，但是接管這台服務器只是時間問題');
+        connection.disconnect();
+    }
+
+    if (message.content === `${prefix}resin`) {
+        getResin(message);
+    }
+
+});
+
+function playStupidVoice(message) {
+    const args = message.content.slice(`${prefix}stupid`.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    switch (command) {
+        case 'neville':
+            var dispatcher = connection.play('sounds/qwert.m4a');
+            break;
+        case 'that':
+            var dispatcher = connection.play('sounds/kleine_kindjes.wav');
+            break;
+        case 'rezero':
+            var dispatcher = connection.play('sounds/PTSD.mp3');
+            break;
+        case 'tay':
+            var dispatcher = connection.play('sounds/Strakke_tay.mp3');
+            break;
+        case 'tay2':
+            var dispatcher = connection.play('sounds/tay_owo.wav');
+            break;
+        case 'tay3':
+            var dispatcher = connection.play('sounds/zo_hardd.wav');
+            break;
+        case 'tay4':
+            var dispatcher = connection.play('sounds/tay4.mp4');
+        default:
+            var dispatcher = connection.play('sounds/qwery.m4a');
+            break
+    }
+
+    dispatcher.on('start', () => {
+        console.log('audio.mp3 is now playing!');
+    });
+
+    dispatcher.on('finish', () => {
+        console.log('audio.mp3 has finished playing!');
+    });
+
+    message.channel.send('akwụna' + command);
+}
