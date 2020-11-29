8e5ba6776a9bf368c8745649139341c3fb4bc3c0
Angelo <7332321+antjowie@users.noreply.github.com>
2020-11-29 22:41:29 +0100

    Added the time at which the resin is capped to the resin commands

--- discord/resin.js
+++ discord/resin.js
@@ -2,6 +2,8 @@ const bent = require('bent')
 
 const getJSON = bent('json')
 const getBuffer = bent('buffer')
+const resinValue = 8 * 60;
+const resinCap = 160;
 
 // let obj = await getJSON('http://site.com/json.api')
 // let buffer = await getBuffer('http://site.com/image.png')
@@ -11,6 +13,25 @@ const getResin = async (id) => await getJSON(`https://resintracker.netlify.app/a
 
 // Set the resin, expected in resin value
 const setResin = async (id, resin) => await getJSON(`https://resintracker.netlify.app/api/setResin?&discord=${id}&resin=${resin}`);
+
+const getTimeAtResinCap = (resinSec) => {
+    const resinLeftSec = resinCap * resinValue - resinSec;
+    // const current = Date(Date().now() + resinLeftSec * 1000);
+    const dateAtCap = new Date(Date.now() + resinLeftSec * 1000);
+    
+    return dateAtCap.toLocaleTimeString();
+    // return `${dateAtCap.toDateString()} ${dateAtCap.toTimeString()}`;
+    // console.log(dateAtCap.toDateString());
+    // console.log(dateAtCap.toTimeString());
+}
+
+const sendResinMessage = (message, resinSec) => {
+    let timeLeft = new Date((resinValue * resinCap - resinSec) * 1000).toISOString().substr(11,8);
+    const author = `<@${message.author.id}>`;
+    const currentResin = Math.floor(resinSec / resinValue);
+    
+    message.channel.send(`${author} **Resin** ${currentResin}/${resinCap} | **Left** ${timeLeft} | **Capped at** ${getTimeAtResinCap(resinSec)}`);
+}
 
 const commandCallback = async (message) => {
     const args = message.content.trim().split(/ +/);
@@ -22,13 +43,9 @@ const commandCallback = async (message) => {
         return message.channel.send(`<@${message.author.id}> Invalid args: get | set resin`);
     }
     
-    const resinValue = 8 * 60;
-    const resinCap = 160;
-    
     if(args[0] === "get") {
         let resinSec = await getResin(message.author.id);
-        let timeLeft = new Date((resinValue * resinCap - resinSec) * 1000).toISOString().substr(11,8);
-        message.channel.send(`<@${message.author.id}> Resin ${Math.floor(resinSec / resinValue)}/${resinCap} ${timeLeft} left`);
+        sendResinMessage(message,resinSec);
     }
     else if (args[0] === "set") {
         // TODO: Data validation
@@ -39,14 +56,15 @@ const commandCallback = async (message) => {
         await setResin(message.author.id, args[1]);
         
         let resinSec = args[1] * resinValue//await getResin(message.author.id);
-        let timeLeft = new Date((resinValue * resinCap - resinSec) * 1000).toISOString().substr(11,8);
-        message.channel.send(`<@${message.author.id}> Resin ${Math.floor(resinSec / resinValue)}/${resinCap} ${timeLeft} left`);
+        sendResinMessage(message,resinSec);
     }
 }
 
 module.exports = {
     commandCallback
 }
+
+// console.log(getTimeAtResinCap(10));
 
 // const test = async() =>
 // {    
