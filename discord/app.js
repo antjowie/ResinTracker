f0f9930ef116761feef34c8d838b2f7d7b06a3a6
Neville <s1130624@student.windesheim.nl>
2020-12-08 21:22:27 +0100

    bot chnages

--- discord/app.js
+++ discord/app.js
@@ -72,18 +72,7 @@ client.on('message', async message => {
         message.channel.send('Pong.');
     }
 
-    if (message.content === `${prefix}berat`) {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('지우다');
-    }
-
-    if (message.content === `${prefix}stop`) {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('好的，我這次會聽，但是接管這台服務器只是時間問題');
-        connection.disconnect();
-    }
-
-    if (message.content === `${prefix}E.N.D`) {
+    if (message.content === `${prefix}stop` || message.content === `${prefix}E.N.D`) {
         // send back "Pong." to the channel the message was sent in
         message.channel.send('好的，我這次會聽，但是接管這台服務器只是時間問題');
         connection.disconnect();
@@ -93,7 +82,44 @@ client.on('message', async message => {
         await resin.commandCallback(message);
     }
 
+    
+    if (message.content.startsWith(`${prefix}royal`)) {
+        message.channel.send(calcRoyalWapons(15,60,100));
+    }
+
 });
+
+function calcRoyalWapons(BaseCritRate,basicDamage,critDamge){
+    var royalBuff = 8;
+    var maxStack = 5;
+    var stacks = 0;
+    var critRate = BaseCritRate;
+    var critCap = 100;
+    var damageDone = 0;
+    var critCount = 0;
+    var critDamge = 100;
+
+    for(var i = 0;i < 100;i++){
+        var hit = Math.floor(Math.random() * Math.floor(critCap));
+        if(hit <= critRate){
+            damageDone += basicDamage * critDamge;
+            critCount++;
+            stacks = 0;
+            critRate = BaseCritRate;
+        }else{
+            damageDone += basicDamage;
+            if(stacks > maxStack){
+                stacks++;
+                critRate += royalBuff;
+                if(critRate > 100){
+                    critRate =  100;
+                }
+            }
+        }
+    }
+
+    return `damage done:${damageDone} crits hit: ${critCount}`;
+}
 
 async function playStupidVoice(message) {
     const args = message.content.slice(`${prefix}stupid`.length).trim().split(/ +/);
