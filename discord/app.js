a83e5e9f90e8b90eb5113e8435c6ea16d874c9cf
Neville <s1130624@student.windesheim.nl>
2020-12-13 23:38:34 +0100

    changed bot messages

--- discord/app.js
+++ discord/app.js
@@ -6,6 +6,7 @@ const { start } = require("repl");
 const { token } = require("../token.json");
 const db = require("./genshin_data.js");
 const dbHandler = require("./database_handler");
+const { table } = require("table");
 dbHandler.initialize(false);
 
 const client = new Discord.Client();
@@ -125,52 +126,44 @@ async function farmHandler(args, userId) {
     if (!args[0]) {
         var team = (await dbHandler.get("resin", userId)).team;
         var day = new Date().getDay();
-        day++;
-        day++;
+        
         if (day === 0) {
-            return ".... you can farm all books today (2) of your characters need to go to IDK we still dont have a location object";
+            return "you can farm all books today";
         }
 
-        var farmCharacters = {};
+        let farmCharacters = [
+            ["Talent", "Name", "Location"]
+        ];
 
         team.forEach(name => {
             var char = genshinDict.chars[name];
             var talent = char.talent;
             if ((day - 1) % 3 === genshinDict.talents[talent]) {
-                if (!farmCharacters[talent]) {
-                    farmCharacters[talent] = { "chars": [] };
-                }
-                farmCharacters[talent].chars.push(name);
+                farmCharacters.push([talent, name, "Still dont have a location object"]);
             }
         });
 
-        var message = [];
+        return "```" + table(farmCharacters) + "```";
 
-        for (const [key, value] of Object.entries(farmCharacters)) {
-            message.push(`'${key}' can befarmed for ${value.chars} at IDK we dont have a location object \n`);
-        };
-
-        return `${message}`;
-    }else if (args[0] === 'add'){
+    } else if (args[0] === 'add') {
         var user = await dbHandler.get("resin", userId);
+
         if (genshinDict.chars[args[1]] && user.team.indexOf(args[1])) {
             user.team.push(args[1]);
             await dbHandler.set("resin", userId, { "team": user.team });
             return `your team has been updated`;
-        }else{
+        } else {
             return `could not find ${args[1]}`
         }
     }
     else if (args[0] === 'set') {
-        args.shift();
         var user = await dbHandler.get("resin", userId);
 
-        for (var i = 0; i < args.length; i++) {
+        for (var i = 1; i < args.length; i++) {
             var char = genshinDict.chars[args[i]];
 
             if (!char) {
                 return `could not find ${args[i]}`;
-
             }
         }
         await dbHandler.set("resin", userId, { "team": args });
@@ -179,7 +172,7 @@ async function farmHandler(args, userId) {
     } else if (args[0] === 'remove') {
         var user = await dbHandler.get("resin", userId);
 
-        for (var i = 0; i < args.length; i++) {
+        for (var i = 1; i < args.length; i++) {
             if (genshinDict.chars[args[i]] && user.team.indexOf(args[i])) {
                 user.team.splice(user.team.indexOf(args[i]), 1);
             } else {
@@ -192,8 +185,7 @@ async function farmHandler(args, userId) {
 
     } else if (args[0] === 'get') {
         var user = await dbHandler.get("resin", userId);
-        return `your team consists of ${user.team}`;
-
+        return `${user.team}`;
     } else {
         return `command could not be found`;
     }
